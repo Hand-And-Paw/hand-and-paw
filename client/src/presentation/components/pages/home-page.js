fe41e792db2e29053fac38dccd5417e624edfbef
Rafael Garcia <rago89@gmail.com>
2021-10-19 09:33:55 +0200

    add static folder index.js update path in call

--- client/src/presentation/components/pages/home-page.js
+++ client/src/presentation/components/pages/home-page.js
@@ -4,7 +4,9 @@ export const homePage = async () => {
   // put the component that belongs to homePage
   const divEl = document.createElement("div");
   const users = await fetchUsers();
-  console.log(users);
+  const img = document.createElement("img");
+  img.src = `../../../../public/avatar-uploads/${users[0].avatar}`;
   divEl.textContent = JSON.stringify(users);
+  divEl.appendChild(img);
   return divEl;
 };
