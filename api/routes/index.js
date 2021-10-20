1cf15e5ba9ff706b32afe39c1d50e0262e6f7643
Rafael Garcia <rago89@gmail.com>
2021-10-20 13:03:04 +0200

    change name in missing file

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const routes = express.Router();
 
 // require routes files
-const userSubscription = require("./sub-routes/user-register");
+const userRegister = require("./sub-routes/user-register");
 const login = require("./sub-routes/login");
 const publishAnimal = require("./sub-routes/publish-animal");
 
 // use them with this router
-routes.use("/user-register", userSubscription);
+routes.use("/user-register", userRegister);
 routes.use("/login", login);
 routes.use("/publish-animal", publishAnimal);
 
