e882a037cc4d74ed4fdc2462631ec4ad90db5bfa
HYF <alinamarasca@gmail.com>
2021-10-23 12:52:52 +0200

    connect index page

--- client/src/presentation/init/find-animal.js
+++ client/src/presentation/init/find-animal.js
@@ -2,7 +2,6 @@ import animalSearchResults from "../components/shared/animal-search-results.js";
 import { fetchAnimals } from "../../data-access/animal-access/fetch-animals.js";
 
 const buildPage = async () => {
-  debugger;
   const array = await fetchAnimals();
   document
     .querySelector(".animal-search-results")
