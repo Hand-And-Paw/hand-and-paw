6b4f5670097e8582e737970f11c6f973e5d0c0a7
HYF <alinamarasca@gmail.com>
2021-10-25 17:48:24 +0200

    few changes

--- client/src/presentation/init/index.js
+++ client/src/presentation/init/index.js
@@ -1,7 +1,8 @@
 import { navbar } from "../components/layout/navbar.js";
 
 const buildPage = () => {
-  document.querySelector(".container").appendChild(navbar());
+  document.getElementById("menu").appendChild(navbar());
+
   console.log("Hello!");
 };
 buildPage();
