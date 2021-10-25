3ad4c819e980e8defc721cfabdb068a5d9e48d1c
Rafael Garcia <rago89@gmail.com>
2021-10-25 08:31:32 +0200

    add path index and update fetch data

--- client/src/data-access/animal-access/fetch-animals.js
+++ client/src/data-access/animal-access/fetch-animals.js
@@ -1,5 +1,10 @@
 import { performFetch } from "../api-calls/calls.js";
 
+/**
+ *
+ * @returns An array of all animals
+ */
+
 export const fetchAnimals = async () => {
-  return performFetch("publish-animal");
+  return performFetch("animals");
 };
