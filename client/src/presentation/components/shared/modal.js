6a65a1678e16ad1a5f2a3bcaf803dea6ec4ccc2e
HYF <alinamarasca@gmail.com>
2021-10-27 00:10:50 +0200

    modal component #139

--- client/src/presentation/components/shared/modal.js
+++ client/src/presentation/components/shared/modal.js
@@ -1,1 +1,17 @@
+const createModal = (content) => {
+  const modalDiv = document.createElement("div");
+  modalDiv.className = "modal-background";
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  const close = document.createElement("span");
+  close.innerText = "X";
+  const modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  modalContent.appendChild(content);
+  modal.appendChild(close);
+  modal.appendChild(modalContent);
+  modalDiv.appendChild(modal);
+  return modalDiv;
+};
 
+export default createModal;
