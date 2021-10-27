d085352dfbc4e06f2f2a6e0f9af0b2e8199755d6
HYF <alinamarasca@gmail.com>
2021-10-27 16:43:48 +0200

    modal, form, root, button, article css #81

--- client/src/presentation/components/shared/login-form.js
+++ client/src/presentation/components/shared/login-form.js
@@ -4,19 +4,20 @@ export const loginForm = () => {
   const form = document.createElement("form");
   form.id = "login-form";
   form.innerHTML = `
-  <h1> Please, log in. </h1>
-  <label for="email">Email:</label><br />
-    <input type="email" name="email" autocomplete="email"  required/><br />
-    <label for="password">Password:</label><br />
+  <h1> Log in </h1>
+  <label for="email">Email</label><br />
+    <input type="email" name="email" placeholder="Enter your e-mail" autocomplete="email"  required/><br />
+    <label for="password">Password</label><br />
     <input
       type="password"
       name="password"
       autocomplete="current-password"
+      placeholder ="Enter your password"
       required
     />
     <br />
-    <button class="open-register-form">Create an account</button>
-    <button class="submit-btn">Log In</button>
+    <button class="link-button open-register-form">Create an account!</button><br>
+    <button class="button submit-form-button">Log In</button>
   `;
 
   form.addEventListener("click", loginFormHandler);
