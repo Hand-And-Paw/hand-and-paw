72cd7b3026e181a20ff0c1741f6a116214d80c08
HYF <alinamarasca@gmail.com>
2021-10-28 20:10:42 +0200

    add log out

--- client/src/presentation/handlers/logout-handler.js
+++ client/src/presentation/handlers/logout-handler.js
@@ -2,18 +2,20 @@
 import state from "../../data-access/state/state.js";
 import { navbar } from "../components/layout/navbar.js";
 
-const logOut = () => {
-  // delete token, change isLoggedIn, change navbar
-  state.token = undefined;
-  state.userId = undefined;
-  state.isLoggedIn = false;
-  localStorage.removeItem("token");
-  localStorage.removeItem("userId");
-  localStorage.removeItem("isLoggedIn");
-  const header = document.getElementById("menu");
-  const navbarEl = document.getElementById("main-navbar");
-  header.removeChild(navbarEl);
-  header.appendChild(navbar());
+const logOut = (event) => {
+  if (event.target.id === "log-out") {
+    // delete token, change isLoggedIn, change navbar
+    state.token = undefined;
+    state.userId = undefined;
+    state.isLoggedIn = false;
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("isLoggedIn");
+    const header = document.getElementById("menu");
+    const navbarEl = document.getElementById("main-navbar");
+    header.removeChild(navbarEl);
+    header.appendChild(navbar());
+  }
 };
 
 export default logOut;
