a4b897c4a30ac6d2cd64cc459b8218ec19e54318
HYF <alinamarasca@gmail.com>
2021-10-28 15:51:42 +0200

    navbar logo

--- client/src/presentation/components/layout/navbar.js
+++ client/src/presentation/components/layout/navbar.js
@@ -2,17 +2,17 @@ import callForm from "../../handlers/call-form.js";
 
 export const navbar = () => {
   const navbarEl = document.createElement("navbar");
-  navbarEl.id = "main-navbar";
+  navbarEl.id = "top-navbar";
   // logo
   const logo = document.createElement("div");
   logo.className = "logo";
   const img = document.createElement("img");
-  img.src = "./#";
+  img.src = "./assets/images/ui/home/figma-images/Logo.png";
   logo.appendChild(img);
   navbarEl.appendChild(logo);
   // menu
   const ul = document.createElement("ul");
-  ul.className = "menu";
+  ul.className = "pages-menu";
   // visitor or logged in user
   const isLoggedIn = localStorage.getItem("isLoggedIn");
 
@@ -37,7 +37,7 @@ export const navbar = () => {
   } else {
     ul.innerHTML = `
   <li> <a href = "./src/presentation/components/pages/find-animal.html">Find an animal</a></li>
-  <li> <button id="register-animal-btn"> Register animal</button></li>
+  <li> <button id="register-animal-btn"> Register an animal</button></li>
   <li> <a href =  "./src/presentation/components/pages/find-shelter.html"> Find a shelter</a></li>
   <li> <a href =  "./src/presentation/components/pages/about-adoption.html"> About adoption</a></li>
   `;
