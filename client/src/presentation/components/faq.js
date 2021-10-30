0217dc2d4a8f5c66a05d0b5560e4d92b2cac8d33
Zehra <zehrayelkenci03@gmail.com>
2021-10-30 21:01:14 +0200

    faq component

---
+++ client/src/presentation/components/faq.js
@@ -0,0 +1,12 @@
+// import { faqListHandler } from "../handlers/faq-handler.js";
+// const faqList = (event) => {
+//     // console.log("hello");
+// // when I click to questions,I will see answers
+//     const question = document.querySelectorAll('.question');
+    
+//     question.addEventListener("click", faqListHandler);
+
+// };
+
+// export default faqList;
+
