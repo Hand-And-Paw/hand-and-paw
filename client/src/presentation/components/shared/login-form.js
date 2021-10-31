999633f444df9bd3c7f07631053a4257b0a947b3
Alina Marasca <75176258+alinamarasca@users.noreply.github.com>
2021-10-31 17:15:35 +0100

    made class for css

--- client/src/presentation/components/shared/login-form.js
+++ client/src/presentation/components/shared/login-form.js
@@ -2,6 +2,7 @@ import loginFormHandler from "../../handlers/login-form-handler.js";
 
 export const loginForm = () => {
   const form = document.createElement("form");
+  form.className = "modal-form";
   form.id = "login-form";
   form.action = "#";
   form.innerHTML = `
