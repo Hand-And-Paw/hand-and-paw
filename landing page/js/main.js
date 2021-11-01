4c12f6d9a68eb5770a2f5ed0ca32b17c92a78f33
OksanaShulha <shushullja@gmail.com>
2021-11-01 23:57:08 +0100

    Add burger menu

--- landing page/js/main.js
+++ landing page/js/main.js
@@ -8,3 +8,18 @@
         }
     };
     }());
+
+
+    //Burger handler
+
+    (function () {
+        const burgerItem = document.querySelector('.burger');
+        const menu = document.querySelector('.header_nav');
+        const menuCloseItem = document.querySelector('.header_nav-close');
+        burgerItem.addEventListener('click', () => {
+            menu.classList.add('header_nav_active');
+        });
+        menuCloseItem.addEventListener('click', () => {
+            menu.classList.remove('header_nav_active');
+        });
+    }());
