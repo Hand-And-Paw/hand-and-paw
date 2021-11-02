ed25186fcea62f70a39f6f72a05f23c39e101737
Rafael Garcia <rago89@gmail.com>
2021-11-02 13:10:53 +0100

    add error structure for users #191

--- api/utils/custom-error.js
+++ api/utils/custom-error.js
@@ -7,10 +7,16 @@ class Error {
 }
 
 class CustomError extends Error {
+<<<<<<< HEAD
   constructor(message, type, code) {
     super(message);
     this.name = `${type}:`;
     this.code = `${code}:`;
+=======
+  constructor(message, code) {
+    super(message); // (1)
+    this.name = `(${code}):`; // (2)
+>>>>>>> add error structure for users #191
   }
 }
 
