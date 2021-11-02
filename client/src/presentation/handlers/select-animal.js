74161d929b36db19b5224d03064c1f12d0954fb1
HYF <alinamarasca@gmail.com>
2021-11-02 19:57:17 +0100

    animal profile page fethcesand shows animal and story

--- client/src/presentation/handlers/select-animal.js
+++ client/src/presentation/handlers/select-animal.js
@@ -3,18 +3,14 @@ import { animalCard } from "../components/shared/animal-card.js";
 //
 
 const selectAnimal = async (e) => {
-  // console.log(e.target.closest(".animal-card").id);
   const animalId = e.target.closest(".animal-card").id;
   const animal = await getAnimal(animalId);
-  console.log(animal);
-  return animal;
+  const main = document.querySelector(".main");
+  main.innerHTML = "";
+  main.appendChild(animalCard(animal[0]));
+  const animalStory = document.createElement("div");
+  animalStory.innerText = animal[0].describeAnimal;
+  main.appendChild(animalStory);
 };
-
-// const showAnimalProfileHandler = () => {
-//   const main = document.querySelector(".main");
-//   const fetchedAnimal = selectAnimal();
-//   main.innerHTML = "";
-//   main.appendChild(animalCard(fetchedAnimal));
-// };
 
 export default selectAnimal;
