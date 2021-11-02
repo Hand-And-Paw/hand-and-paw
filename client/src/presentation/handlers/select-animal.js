06cb79a003383c47e56e783004d39579465158cc
HYF <alinamarasca@gmail.com>
2021-11-02 18:59:56 +0100

    experiment with function

--- client/src/presentation/handlers/select-animal.js
+++ client/src/presentation/handlers/select-animal.js
@@ -1,4 +1,6 @@
 import { getAnimal } from "../../data-access/animal-access/get-animal.js";
+import { animalCard } from "../components/shared/animal-card.js";
+//
 
 const selectAnimal = async (e) => {
   // console.log(e.target.closest(".animal-card").id);
@@ -7,5 +9,12 @@ const selectAnimal = async (e) => {
   console.log(animal);
   return animal;
 };
+
+// const showAnimalProfileHandler = () => {
+//   const main = document.querySelector(".main");
+//   const fetchedAnimal = selectAnimal();
+//   main.innerHTML = "";
+//   main.appendChild(animalCard(fetchedAnimal));
+// };
 
 export default selectAnimal;
