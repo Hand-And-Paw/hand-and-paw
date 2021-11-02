dd7e4043489f6db17fa99f40dee2be2d956367ba
HYF <alinamarasca@gmail.com>
2021-11-02 13:30:48 +0100

    fix class in navbar, change handler name to call login form

--- client/src/presentation/components/layout/login-signup-menu.js
+++ client/src/presentation/components/layout/login-signup-menu.js
@@ -1,13 +1,14 @@
-import callForm from "../../handlers/call-form.js";
+import callLoginForm from "../../handlers/call-login-form.js";
 
 const createLoginSignupMenu = () => {
   const loginSignupMenu = document.createElement("div");
   loginSignupMenu.className = "account-menu";
   const button = document.createElement("button");
-  button.id = "account-menu";
+  button.id = "account-menu-btn";
   button.innerText = "Log in / Sign up";
   loginSignupMenu.appendChild(button);
-  loginSignupMenu.addEventListener("click", callForm);
+  loginSignupMenu.addEventListener("click", callLoginForm);
+  // loginSignupMenu.addEventListener("click", console.log("i work"));
   return loginSignupMenu;
 };
 
