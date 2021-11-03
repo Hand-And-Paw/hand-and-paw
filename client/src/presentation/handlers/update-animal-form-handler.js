ed2efa8f58d8527c092c8d1e84be4a1cddf50eb4
Rafael Garcia <rago89@gmail.com>
2021-11-03 16:13:07 +0100

    add connection to update animal

--- client/src/presentation/handlers/update-animal-form-handler.js
+++ client/src/presentation/handlers/update-animal-form-handler.js
@@ -3,17 +3,20 @@ import createModal from "../components/shared/modal.js";
 import closeModal from "./close-modal.js";
 
 export const updateAnimalFormHandler = async (event) => {
-  event.preventDefault();
   const form = document.getElementById("register-animal-form");
+  const animalId = event.target.closest(".animal-card").id;
+  event.preventDefault();
+
   const formData = new FormData(form);
 
+  formData.append("id", animalId);
   formData.append("userId", window.localStorage.getItem("userId"));
 
   const post = await updateAnimal(formData);
   if (post) {
     const divEl = document.createElement("div");
     const title = document.createElement("h1");
-    title.innerText = "Animal registered successfully";
+    title.innerText = "Animal updated successfully";
     const anchor = document.createElement("a");
     anchor.href = "#";
     anchor.innerHTML = "go to your animal card";
