eff052a8be5c139f97bfa73ef1553c0b2ed3563b
HYF <alinamarasca@gmail.com>
2021-11-03 12:22:34 +0100

    add contact shelter-button

--- client/src/presentation/handlers/show-animal-profile.js
+++ client/src/presentation/handlers/show-animal-profile.js
@@ -1,5 +1,6 @@
 import { getAnimal } from "../../data-access/animal-access/get-animal.js";
 import { animalCard } from "../components/shared/animal-card.js";
+import contactShelter from "../components/layout/contact-shelter-button.js";
 
 const showAnimalProfile = async (e) => {
   const animalId = e.target.closest(".animal-card").id;
@@ -7,6 +8,7 @@ const showAnimalProfile = async (e) => {
   const main = document.querySelector(".main");
   main.innerHTML = "";
   main.appendChild(animalCard(animal[0]));
+  main.appendChild(contactShelter());
   const animalStory = document.createElement("div");
   animalStory.innerText = animal[0].describeAnimal;
   main.appendChild(animalStory);
