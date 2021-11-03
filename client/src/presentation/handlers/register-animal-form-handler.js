479e62a4368d5def57f9b462fbe47e89d59d9ddb
Rafael Garcia <rago89@gmail.com>
2021-11-03 19:37:21 +0100

    add rendercard after register pet

--- client/src/presentation/handlers/register-animal-form-handler.js
+++ client/src/presentation/handlers/register-animal-form-handler.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 import { registerAnimal } from "../../data-access/animal-access/register-animal.js";
 import createModal from "../components/shared/modal.js";
+import closeModal from "./close-modal.js";
+import state from "../../data-access/state/state.js";
+import showAnimalProfile from "./show-animal-profile.js";
 
 export const registerAnimalFormHandler = async (event) => {
   event.preventDefault();
@@ -10,18 +13,24 @@ export const registerAnimalFormHandler = async (event) => {
   formData.append("userId", window.localStorage.getItem("userId"));
 
   const post = await registerAnimal(formData);
-  if (post._id) {
+  state.animalId = post._id;
+  if (post) {
     const divEl = document.createElement("div");
     const title = document.createElement("h1");
     title.innerText = "Animal registered successfully";
     const anchor = document.createElement("a");
     anchor.href = "#";
     anchor.innerHTML = "go to your animal card";
+    anchor.addEventListener("click", (e) =>
+      showAnimalProfile(e, state.animalId)
+    );
+    // delete back to search result option
     divEl.appendChild(title);
     divEl.appendChild(anchor);
     document
       .querySelector("body")
       .insertAdjacentElement("beforeend", createModal(divEl));
+    setTimeout(closeModal, 4000);
     form.reset();
   }
 };
