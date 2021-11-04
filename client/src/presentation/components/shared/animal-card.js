52a90c38c49f21d418c87e62924622ee0505a479
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2021-11-04 19:07:05 +0100

    Merge changed files

--- client/src/presentation/components/shared/animal-card.js
+++ client/src/presentation/components/shared/animal-card.js
@@ -1,37 +1,16 @@
-import getAnimalPrincipalPicture from "../../../business-logic/get-animal-principal-picture.js";
-import { b64toBlob } from "../../../business-logic/base-to-blob.js";
+import animalPhoto from "./animal-photo.js";
+import animalInfo from "./animal-info.js";
 
-export const animalCard = (animal, className) => {
-  const { type, breed, gender, character, dateBirth, pictures, location } =
-    animal;
+export const animalCard = (animal, className, id) => {
   // create card
+  const { _id } = animal;
   const card = document.createElement("div");
+  card.id = _id;
   card.className = "animal-card";
   card.classList.add(className);
   // create photo div
-  const photo = document.createElement("div");
-  photo.className = "card-photo";
-  const img = document.createElement("img");
-  // looks for principal picture
-  const picture = getAnimalPrincipalPicture(pictures);
-  const blob = b64toBlob(picture.picture.data, picture.picture.contentType);
-  const url = URL.createObjectURL(blob);
-  img.src = url;
-  photo.appendChild(img);
+  card.appendChild(animalPhoto(animal, "card-photo"));
   // create info div
-  const info = document.createElement("div");
-  info.className = "card-info";
-  info.innerHTML = `
-  Name: <span> ${animal.name} </span> <br>
-  Type: <span> ${type} </span> <br>
-  Breed: <span> ${breed} </span> <br>
-  Gender: <span> ${gender}</span><br>
-  Character: <span>${character}</span><br>
-  Date of Birth: <span> ${dateBirth} </span><br>
-  Location: <span> ${location}</span> <br>
-  `;
-  // append div
-  card.appendChild(photo);
-  card.appendChild(info);
+  card.appendChild(animalInfo(animal, "card-info"));
   return card;
 };
