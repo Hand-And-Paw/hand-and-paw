4a83e920cf12bf95f900ea9bd7bd2878f56d903b
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2021-11-04 18:40:43 +0100

    Enhance footer css

--- client/src/presentation/components/layout/github-link.js
+++ client/src/presentation/components/layout/github-link.js
@@ -1,10 +1,15 @@
 const githubRepo = () => {
+  const githubRepo = document.createElement("div");
+  githubRepo.className = "githubrepo"
   const anchor = document.createElement("a");
   anchor.href = "https://github.com/Hand-And-Paw/hand-and-paw";
+  anchor.target = "blank";
   const icon = document.createElement("img");
+  icon.className="github-icon"
   icon.src = "/assets/icons/footer/akar-icons_github-fill.svg";
   anchor.appendChild(icon);
-  return anchor;
+  githubRepo.appendChild(anchor);
+  return githubRepo;
 };
 
 export default githubRepo;
