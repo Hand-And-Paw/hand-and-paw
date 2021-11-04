708a9ffc3c0c3c9ac6b39a093ef27d0a82e154c4
HYF <alinamarasca@gmail.com>
2021-11-04 18:14:46 +0100

    add button class for styling

--- client/src/presentation/components/layout/contact-shelter-button.js
+++ client/src/presentation/components/layout/contact-shelter-button.js
@@ -2,6 +2,7 @@ const contactShelter = () => {
   const button = document.createElement("button");
   button.id = "contact-shelter";
   button.innerText = "Get in touch";
+  button.className = "form-button";
   button.classList.add("button");
   button.addEventListener("click", () =>
     console.log("working contact shelter listener")
