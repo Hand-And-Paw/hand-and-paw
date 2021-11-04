1305012b37951c6dcd1bc0f32d1c62260b546446
HYF <alinamarasca@gmail.com>
2021-11-04 19:36:45 +0100

    fix listener

--- client/src/presentation/components/layout/contact-shelter-button.js
+++ client/src/presentation/components/layout/contact-shelter-button.js
@@ -1,9 +1,9 @@
 import openModal from "../../handlers/call-login-form.js";
 import contactForm from "./contact-form.js";
 
-const call = () => {
-  openModal(contactForm("Get in touch", "contact-shelter"));
-};
+// const call = () => {
+//   openModal(contactForm("Get in touch", "contact-shelter"));
+// };
 
 const contactShelter = () => {
   const button = document.createElement("button");
@@ -11,12 +11,10 @@ const contactShelter = () => {
   button.innerText = "Get in touch";
   button.className = "form-button";
   button.classList.add("button");
-  button.addEventListener("click", call);
+  button.addEventListener("click", () => {
+    openModal(contactForm("Get in touch", "contact-shelter"));
+  });
   return button;
 };
 
 export default contactShelter;
-
-// loginSignupMenu.addEventListener("click", () => {
-//   openModal(loginForm("modal-form"));
-// });
