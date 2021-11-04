d0a61fa5cbe9948aadd8294af93b60a968b977f2
HYF <alinamarasca@gmail.com>
2021-11-04 12:35:06 +0100

    delete commented out code

--- client/src/presentation/components/layout/login-signup-menu.js
+++ client/src/presentation/components/layout/login-signup-menu.js
@@ -7,13 +7,7 @@ const createLoginSignupMenu = () => {
   const button = document.createElement("button");
   button.id = "account-menu-btn";
   button.innerText = "Log in / Sign up";
-  // const avatar = document.createElement("div");
-  // avatar.className = "avatar";
-  // const avatarImage = document.createElement("img");
-  // avatarImage.src = "/assets/icons/header_codicon_account.svg";
-  // avatar.appendChild(avatarImage);
   loginSignupMenu.appendChild(button);
-  // loginSignupMenu.appendChild(avatar);
   loginSignupMenu.addEventListener("click", () => {
     openModal(loginForm("modal-form"));
   });
