1c4e43e00804e079da190de3ca20a65fa3583a3a
HYF <alinamarasca@gmail.com>
2021-11-07 09:00:55 +0100

    add footer and navbar

--- client/src/presentation/init/favorites.js
+++ client/src/presentation/init/favorites.js
@@ -1,7 +1,11 @@
 import animalSearchResults from "../components/shared/animal-search-results.js";
 import { fetchAnimals } from "../../data-access/animal-access/fetch-animals.js";
+import { navbar } from "../components/layout/navbar.js";
+import footer from "../components/layout/footer.js";
 
 const buildPage = async () => {
+  document.getElementById("menu").appendChild(navbar());
+  document.querySelector("footer").appendChild(footer());
   // fetch favorite animals
   const array = await fetchAnimals();
   document
