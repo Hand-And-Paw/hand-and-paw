812c3a9c9b16866d5ba69d6f08e9416c93a3c5e8
HYF <alinamarasca@gmail.com>
2021-11-10 21:20:35 +0100

    small fixes on styling

--- client/src/presentation/components/layout/profile-dropdown-menu.js
+++ client/src/presentation/components/layout/profile-dropdown-menu.js
@@ -3,16 +3,16 @@ const createProfileDropDownMenu = () => {
   profileMenu.id = "navbar-dropdown";
   profileMenu.className = "dropdown-content";
   profileMenu.innerHTML = `
-  <div id="show-favorites">
-  <a href="/src/presentation/components/pages/favorites.html">Favorites</a><br>
+  <div id="show-favorites" class="dropdown-menu-item">
+  <a href="/src/presentation/components/pages/favorites.html">Favorites</a>
   <img src="/assets/icons/dropdown menu/akar-icons_heart.svg" alt="Favorites"> 
 </div>
-<div id="show-my-animals">
-  <a href="/src/presentation/components/pages/my-animals.html">My animals</a><br>
+<div id="show-my-animals" class="dropdown-menu-item">
+  <a href="/src/presentation/components/pages/my-animals.html">My animals</a>
   <img src="/assets/icons/dropdown menu/ph_dog.svg" alt="Animal"> 
 </div>
-<div id="edit-profile">
-  <a href="/src/presentation/components/pages/edit-user-profile.html">Edit profile</a><br>
+<div id="edit-profile" class="dropdown-menu-item">
+  <a href="/src/presentation/components/pages/edit-user-profile.html">Edit profile</a>
   <img src="/assets/icons/dropdown menu/dropdownmenu_codicon_account.svg" alt="Avatar"> 
 </div>
 `;
