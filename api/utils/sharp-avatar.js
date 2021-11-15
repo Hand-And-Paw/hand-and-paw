000362c21697948fa1b86169263a57d0b1b9fbca
Rafael Garcia <rago89@gmail.com>
2021-11-15 09:35:07 +0100

    fit content animal Card #357

--- api/utils/sharp-avatar.js
+++ api/utils/sharp-avatar.js
@@ -2,7 +2,7 @@ const sharp = require("sharp");
 
 const compressSharpAvatar = async (imagePath) => {
   const a = await sharp(imagePath)
-    .resize(200, 800, {
+    .resize(200, 200, {
       kernel: sharp.kernel.nearest,
       fit: "inside",
       background: { r: 255, g: 255, b: 255, alpha: 0.5 },
