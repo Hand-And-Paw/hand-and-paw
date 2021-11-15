d70f1eb3938b0cbee00b19f24c0a0b05f5e0b2ab
HYF <alinamarasca@gmail.com>
2021-11-15 10:56:56 +0100

    merge from development

--- client/src/presentation/components/shared/animal-info.js
+++ client/src/presentation/components/shared/animal-info.js
@@ -5,21 +5,18 @@
  */
 
 const animalInfo = (animal, className) => {
-  const { type, breed, gender, character, age, location, province } = animal;
+  const { type, breed, gender, character, age, location } = animal;
   const info = document.createElement("div");
   info.className = className;
   info.innerHTML = `
     
     <div class="animal-name"> <h3>${animal.name}</h3> </div> <br>
-    Type: <span>${type}</span> <br>
-    Breed: <span>${breed}</span> <br>
-    Gender: <span>${gender}</span><br>
+    Type: <span> ${type} </span> <br>
+    Breed: <span> ${breed} </span> <br>
+    Gender: <span> ${gender}</span><br>
     Character: <span>${character}</span><br>
-    Age: <span>${age}</span><br>
-    City: <span>${location}</span> <br>
-    Province: <span>${
-      [...province][0].toUpperCase() + [...province].slice(1).join("")
-    }</span> <br>
+    Age: <span> ${age} </span><br>
+    Location: <span> ${location}</span> <br>
     `;
   return info;
 };
