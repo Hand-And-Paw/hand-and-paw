081556d068dcbdd43a28843684c9de7c88afb382
HYF <alinamarasca@gmail.com>
2021-11-17 22:38:18 +0100

    responsive menu - add space in log out

--- client/src/presentation/components/layout/responsive-menu-component.js
+++ client/src/presentation/components/layout/responsive-menu-component.js
@@ -2,87 +2,84 @@ import openModal from "../../handlers/call-login-form.js";
 import { loginForm } from "../shared/login-form.js";
 import logOut from "../../handlers/logout-handler.js";
 
-
 const responsiveMenuComponent = (props) => {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
-const responsiveMenu = document.createElement("div");
-responsiveMenu.classList.add('responsive-menu');
+  const responsiveMenu = document.createElement("div");
+  responsiveMenu.classList.add("responsive-menu");
 
-// Find animal
-const findAnimalDiv = document.createElement("div");
-findAnimalDiv.className="responsive-menu_item";
-const findAnimalAnchor = document.createElement("a");
-findAnimalAnchor.className="header_link";
-findAnimalAnchor.href = "/src/presentation/components/pages/find-animal.html";
-findAnimalAnchor.innerHTML = "Find an animal";
-findAnimalDiv.appendChild(findAnimalAnchor);
-responsiveMenu.appendChild(findAnimalDiv);
-// Register animal
-const registerAnimalDiv = document.createElement("div");
-registerAnimalDiv.className="responsive-menu_item";
-const registerAnimalAnchor = document.createElement("a");
-registerAnimalAnchor.className="header_link";
-registerAnimalAnchor.id = "register-animal-navBar";
-registerAnimalAnchor.href =
-  "/src/presentation/components/pages/add-animal.html";
-registerAnimalAnchor.innerHTML = "Register an animal";
-if (!isLoggedIn) {
-  registerAnimalAnchor.href = "#";
-  registerAnimalDiv.addEventListener("click", () =>
-    openModal(loginForm("modal-form"))
-  );
-}
-registerAnimalDiv.appendChild(registerAnimalAnchor);
-responsiveMenu.appendChild(registerAnimalDiv);
-// About adoption
-const aboutAdoptionDiv = document.createElement("div");
-aboutAdoptionDiv.className="responsive-menu_item";
-const aboutAdoptionAnchor = document.createElement("a");
-aboutAdoptionAnchor.className="header_link";
-aboutAdoptionAnchor.href =
+  // Find animal
+  const findAnimalDiv = document.createElement("div");
+  findAnimalDiv.className = "responsive-menu_item";
+  const findAnimalAnchor = document.createElement("a");
+  findAnimalAnchor.className = "header_link";
+  findAnimalAnchor.href = "/src/presentation/components/pages/find-animal.html";
+  findAnimalAnchor.innerHTML = "Find an animal";
+  findAnimalDiv.appendChild(findAnimalAnchor);
+  responsiveMenu.appendChild(findAnimalDiv);
+  // Register animal
+  const registerAnimalDiv = document.createElement("div");
+  registerAnimalDiv.className = "responsive-menu_item";
+  const registerAnimalAnchor = document.createElement("a");
+  registerAnimalAnchor.className = "header_link";
+  registerAnimalAnchor.id = "register-animal-navBar";
+  registerAnimalAnchor.href =
+    "/src/presentation/components/pages/add-animal.html";
+  registerAnimalAnchor.innerHTML = "Register an animal";
+  if (!isLoggedIn) {
+    registerAnimalAnchor.href = "#";
+    registerAnimalDiv.addEventListener("click", () =>
+      openModal(loginForm("modal-form"))
+    );
+  }
+  registerAnimalDiv.appendChild(registerAnimalAnchor);
+  responsiveMenu.appendChild(registerAnimalDiv);
+  // About adoption
+  const aboutAdoptionDiv = document.createElement("div");
+  aboutAdoptionDiv.className = "responsive-menu_item";
+  const aboutAdoptionAnchor = document.createElement("a");
+  aboutAdoptionAnchor.className = "header_link";
+  aboutAdoptionAnchor.href =
     "/src/presentation/components/pages/about-adoption.html";
-aboutAdoptionAnchor.innerHTML = "About adoption";
-aboutAdoptionDiv.appendChild(aboutAdoptionAnchor);
-responsiveMenu.appendChild(aboutAdoptionDiv);
+  aboutAdoptionAnchor.innerHTML = "About adoption";
+  aboutAdoptionDiv.appendChild(aboutAdoptionAnchor);
+  responsiveMenu.appendChild(aboutAdoptionDiv);
 
-// Login / Logout
-if (isLoggedIn) {
-  const spacer1 = document.createElement("div");
-  spacer1.className="responsive-spacer_item";
-  spacer1.innerHTML = "&nbsp;";
-  responsiveMenu.appendChild(spacer1);
+  // Login / Logout
+  if (isLoggedIn) {
+    const spacer1 = document.createElement("div");
+    spacer1.className = "responsive-spacer_item";
+    spacer1.innerHTML = "&nbsp;";
+    responsiveMenu.appendChild(spacer1);
 
-  const favoritesMenuItem = document.createElement("div");
-  favoritesMenuItem.className="responsive-sub-menu_item";
-  const favoritesAnchor = document.createElement("a");
-  favoritesAnchor.className="header_link";
-  favoritesAnchor.href =
-    "/src/presentation/components/pages/favorites.html";
-  favoritesAnchor.innerHTML = "Favorites";
-  favoritesMenuItem.appendChild(favoritesAnchor);
-  responsiveMenu.appendChild(favoritesMenuItem);
+    const favoritesMenuItem = document.createElement("div");
+    favoritesMenuItem.className = "responsive-sub-menu_item";
+    const favoritesAnchor = document.createElement("a");
+    favoritesAnchor.className = "header_link";
+    favoritesAnchor.href = "/src/presentation/components/pages/favorites.html";
+    favoritesAnchor.innerHTML = "Favorites";
+    favoritesMenuItem.appendChild(favoritesAnchor);
+    responsiveMenu.appendChild(favoritesMenuItem);
 
-  const myAnimalsMenuItem = document.createElement("div");
-  myAnimalsMenuItem.className="responsive-sub-menu_item";
-  const myAnimalsAnchor = document.createElement("a");
-  myAnimalsAnchor.className="header_link";
-  myAnimalsAnchor.href =
-    "/src/presentation/components/pages/my-animals.html";
+    const myAnimalsMenuItem = document.createElement("div");
+    myAnimalsMenuItem.className = "responsive-sub-menu_item";
+    const myAnimalsAnchor = document.createElement("a");
+    myAnimalsAnchor.className = "header_link";
+    myAnimalsAnchor.href = "/src/presentation/components/pages/my-animals.html";
     myAnimalsAnchor.innerHTML = "My animals";
-  myAnimalsMenuItem.appendChild(myAnimalsAnchor);
-  responsiveMenu.appendChild(myAnimalsMenuItem);
-  
-  const editProfileMenuItem = document.createElement("div");
-  editProfileMenuItem.className="responsive-sub-menu_item";
-  const editProfileAnchor = document.createElement("a");
-  editProfileAnchor.className="header_link";
-  editProfileAnchor.href =
-    "/src/presentation/components/pages/edit-user-profile.html";
+    myAnimalsMenuItem.appendChild(myAnimalsAnchor);
+    responsiveMenu.appendChild(myAnimalsMenuItem);
+
+    const editProfileMenuItem = document.createElement("div");
+    editProfileMenuItem.className = "responsive-sub-menu_item";
+    const editProfileAnchor = document.createElement("a");
+    editProfileAnchor.className = "header_link";
+    editProfileAnchor.href =
+      "/src/presentation/components/pages/edit-user-profile.html";
     editProfileAnchor.innerHTML = "Edit profile";
     editProfileMenuItem.appendChild(editProfileAnchor);
-  responsiveMenu.appendChild(editProfileMenuItem);
-  
-  /* 
+    responsiveMenu.appendChild(editProfileMenuItem);
+
+    /* 
   profileMenu.id = "navbar-dropdown";
   profileMenu.className = "responsive-menu_item";
   profileMenu.innerHTML = `
@@ -102,49 +99,49 @@ if (isLoggedIn) {
 responsiveMenu.appendChild(profileMenu);
  */
 
-  const spacer2 = document.createElement("div");
-  spacer2.className="responsive-spacer_item";
-  spacer2.innerHTML = "&nbsp;";
-  responsiveMenu.appendChild(spacer2);
+    const spacer2 = document.createElement("div");
+    spacer2.className = "responsive-spacer_item";
+    spacer2.innerHTML = "&nbsp;";
+    responsiveMenu.appendChild(spacer2);
 
-  const logoutDiv = document.createElement("div");
-  logoutDiv.className="responsive-menu_item";
-  const logoutLink = document.createElement("a");
-  logoutLink.id = 'log-out';
-  logoutLink.href =
+    const logoutDiv = document.createElement("div");
+    logoutDiv.className = "responsive-menu_item";
+    const logoutLink = document.createElement("a");
+    logoutLink.id = "log-out";
+    logoutLink.href = "/index.html";
+    logoutLink.className = "header_link";
+    logoutLink.innerHTML = "Log out";
+    logoutLink.addEventListener("click", async () => {
+      logOut(event);
+    });
+    logoutDiv.appendChild(logoutLink);
+    responsiveMenu.appendChild(logoutDiv);
+  } else {
+    const loginDiv = document.createElement("div");
+    loginDiv.className = "responsive-menu_item";
+    const loginLink = document.createElement("a");
+    loginLink.id = "log-in";
+    /*   loginLink.href =
   "/index.html";
-  logoutLink.className="header_link";
-  logoutLink.innerHTML = "Logout";
-  logoutLink.addEventListener("click", async () => {logOut(event)} );
-  logoutDiv.appendChild(logoutLink);
-  responsiveMenu.appendChild(logoutDiv);
+ */ loginLink.className = "header_link";
+    loginLink.innerHTML = "Log in / Sign up";
+    loginLink.addEventListener("click", () => {
+      openModal(loginForm("modal-form"));
+    });
+    loginDiv.appendChild(loginLink);
+    responsiveMenu.appendChild(loginDiv);
+  }
 
-
-} else {
-  const loginDiv = document.createElement("div");
-  loginDiv.className="responsive-menu_item";
-  const loginLink = document.createElement("a");
-  loginLink.id = 'log-in';
-/*   loginLink.href =
-  "/index.html";
- */  loginLink.className="header_link";
-  loginLink.innerHTML = "Log in / Sign up";
-  loginLink.addEventListener("click", () => {openModal(loginForm("modal-form"))} );
-  loginDiv.appendChild(loginLink);
-  responsiveMenu.appendChild(loginDiv);
-}
-
-
-const responsiveMenuClose = document.createElement("div");
-responsiveMenuClose.classList.add('responsive-menu-close');
-responsiveMenuClose.innerHTML = `
+  const responsiveMenuClose = document.createElement("div");
+  responsiveMenuClose.classList.add("responsive-menu-close");
+  responsiveMenuClose.innerHTML = `
   <span class="pages-menu-close-line"></span>
   <span class="pages-menu-close-line"></span>
 `;
 
-responsiveMenu.appendChild(responsiveMenuClose);
+  responsiveMenu.appendChild(responsiveMenuClose);
 
-/* 
+  /* 
   // Init properties
   let className = props?.className || "pages-menu";
   // Create component
