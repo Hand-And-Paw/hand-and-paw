a8311ddfdf5d34067b6dec05024a1d0d38ce3035
Rafael Garcia <rago89@gmail.com>
2021-11-19 18:33:33 +0100

    add login if user click haerat and is not loggedin

--- client/src/presentation/handlers/add-to-favorites-handler.js
+++ client/src/presentation/handlers/add-to-favorites-handler.js
@@ -1,8 +1,14 @@
 import { addFavorite } from "../../data-access/user-access/add-favorite.js";
 import { removeFavorite } from "../../data-access/user-access/remove-favorite.js";
+import openModal from "./call-login-form.js";
+import { loginForm } from "../components/shared/login-form.js";
 
 const addToFavoritesHandler = async (event) => {
   event.stopPropagation();
+  if (!sessionStorage.getItem("isLoggedIn")) {
+    openModal(loginForm("modal-form"));
+    return;
+  }
   const { target } = event;
   // change front-end
   const imgFile = target.closest("#heart").src.split("/").pop();
