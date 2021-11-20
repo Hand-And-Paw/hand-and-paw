8bca36df756df3b297c523cc5827a61173ac7052
Rafael Garcia <rago89@gmail.com>
2021-11-20 08:39:47 +0100

    last functionality before presentation, add acces hour users form

--- client/src/presentation/components/shared/public-access-hours.js
+++ client/src/presentation/components/shared/public-access-hours.js
@@ -1,33 +1,39 @@
 /* eslint-disable no-continue */
-export const publicAccessHoursComponent = (obj, animalName) => {
+export const publicAccessHoursComponent = (user, animal) => {
   const divEl = document.createElement("div");
   divEl.id = "public-access-component";
   divEl.className = "animal-public-access";
   const titleEl = document.createElement("h3");
-  titleEl.innerText = `More about ${animalName}'s owner`;
+  titleEl.innerText = `More about ${animal.name}'s owner`;
   const ulEl = document.createElement("ul");
   ulEl.className = "access-days-component";
-  const liTitle = document.createElement("li");
-  liTitle.innerText = "Public Access";
-  liTitle.className = "li-public-access-hour";
-  ulEl.appendChild(liTitle);
-  for (const [key, values] of Object.entries(obj.publicAccess)) {
-    if (!values.hours) {
-      continue;
+
+  if (user.publicAccess) {
+    const liTitle = document.createElement("li");
+    liTitle.innerText = "Public Access";
+    liTitle.className = "li-public-access-hour";
+    ulEl.appendChild(liTitle);
+    for (const [key, values] of Object.entries(user.publicAccess)) {
+      if (!values.hours) {
+        continue;
+      }
+      const liEl = document.createElement("li");
+      liEl.id = `${key}-hours-access`;
+      liEl.innerText = `${key} - ${values.hours}`;
+      liEl.className = "access-day";
+      ulEl.appendChild(liEl);
     }
-    const liEl = document.createElement("li");
-    liEl.id = `${key}-hours-access`;
-    liEl.innerText = `${key} - ${values.hours}`;
-    liEl.className = "access-day";
-    ulEl.appendChild(liEl);
   }
-  if (obj.website) {
+  if (user.website || animal.webSite) {
     const liTitleWebsite = document.createElement("li");
     liTitleWebsite.innerText = "Website";
     liTitleWebsite.className = "li-webSite-hour";
     ulEl.appendChild(liTitleWebsite);
     const liWebsite = document.createElement("li");
-    liWebsite.innerText = obj.website;
+    const a = document.createElement("a");
+    a.href = user.website;
+    a.innerText = user.website;
+    liWebsite.appendChild(a);
     ulEl.appendChild(liWebsite);
   }
   divEl.appendChild(titleEl);
